f6f9bc1cc7218d7df63e759312b9416d114e47d6
”Tóth <sandor.toth@hotmail.com>
2021-01-16 22:18:19 +0100

    array traversal tasks

--- js_tombok_bejarasa.js
+++ js_tombok_bejarasa.js
@@ -1,4 +1,4 @@
-let arr = [1, 5, 77, 45, 16, 3, 8, 99, 10, 89];
+let arr = [1, 5, 77, 45, 'sasa', 3, 8, 99, 10, 89];
 console.log(arr)
 
 //1
@@ -18,7 +18,70 @@ searchArray(22, arr);
 
 function doubleArray(array) {
     for (let i = 0; i < array.length; i++) {
-        array[i] = 2 * array[i];
+        if (typeof array[i] == "number") {
+            array[i] = 2 * array[i];
+            console.log(array[i]);
+        } else {
+            continue;
+        }
+
     }
     return array;
+}
+
+doubleArray(arr);
+
+//3
+
+function calcArrayaverage(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum = array[i] + sum;
+    }
+    return sum / array.length;
+}
+
+//4a
+
+function randomArray(length) {
+    let array = new Array(length);
+    for (let i = 0; i < length; i++) {
+        array[i] = Math.floor(Math.random() * 100);
+    }
+    return array
+}
+
+
+function isMatchinArrays(array1, array2) {
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array1.length; j++) {
+            if (array1[i] == array2[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+let a = [28, 39, 54, 54, 73, 80, 90, 93, 94, 94];
+let b = [14, 16, 25, 54, 60, 81, 82, 86, 88, 98];
+
+console.log(isMatchinArrays(a, b));
+
+
+//4b
+
+function showMatchinArrays(array1, array2) {
+    let array3 = [];
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array1.length; j++) {
+            if (array1[i] == array2[j]) {
+                array3.push(array1[i])
+            }
+        }
+    }
+    return array3;
+}
+
+console.log(showMatchinArrays(a, b));
 
